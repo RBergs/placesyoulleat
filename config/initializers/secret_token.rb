78193c4f504969f06b93f58cd4e48982dabf1756
Richard Berger <richard.h.berger@gmail.com>
2011-12-20 19:47:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlacesYoullEat::Application.config.secret_token = '5054f65b8115c990223c4699dec41c9fdd13d9bdc7b3fa748612c3c80300944d799a271dc9c5cc1594c5925619890ef9f6bb2237ee54098502999caecab2004a'
